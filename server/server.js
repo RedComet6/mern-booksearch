84d02c1a4678fe7efd5fb52a8e5f29f79b6f86ab
Aidan Chamberlain <asunshine99@gmail.com>
2022-04-28 21:56:11 -0500

    created client side query and show data to screen

--- server/server.js
+++ server/server.js
@@ -13,15 +13,19 @@ const server = new ApolloServer({
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../client/build")));
-// }
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 // app.use(routes);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
